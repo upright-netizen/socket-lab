db8736b431a604803e9442c9b3b6d3feb928fbd4
Nathan Stilwell <nathanstilwell@gmail.com>
2014-04-09 22:08:56 -0400

    testing close events

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -4,14 +4,29 @@
 (function(global, document, $, SocketWrench, undefined){
   'use strict';
 
-  var s = new SocketWrench('ws://localhost:4014');
+  var
+    defaults = {
+      url : 'ws://localhost:4014',
+      autoConnect : false
+    },
+    code = 1000,
+    reason = 'Closing normally',
+    receivedCode,
+    receivedReason,
+    s = new SocketWrench(defaults);
 
-  s.on('open', function (what) {
-    console.log('socket opened', what);
+  s.on('ready', function onReady () {
+    s.close(code, reason);
   });
 
-  s.on('close', function onClose (what) {
-    console.log('socket closed', what);
+  s.on('close', function onClose (e) {
+    console.log(e);
+    console.log('expect', (typeof e), 'toBe', 'object');
+    console.log('expect', e.code, 'toBe', code);
+    console.log('expect', e.reason, 'toBe', reason);
   });
+
+  s.open();
+
 
 }(window, window.document, window.jQuery, window.SocketWrench));
